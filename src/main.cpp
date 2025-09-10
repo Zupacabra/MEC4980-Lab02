e749583bcfc76f961e0dc3d3f3761fa75c844250
Zupacabra <zupanm@msoe.edu>
2025-09-10 12:10:22 -0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,12 +4,27 @@
 int myFunction(int, int);
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200); // other common is 9600 - this is comm speed
+  Serial.println("Starting Reads");
+  digitalWrite(LED_BUILTIN, 1);
 }
 
+int ledBrightness = 0;
+int ledValue = 0;
+
 void loop() {
-  // put your main code here, to run repeatedly:
+  delay(250);
+  int reading = analogRead(A0);
+  ledValue = 255 - (int)((reading / 4095.0) * 255);
+
+  Serial.print("Result: ");
+  Serial.print(reading);
+  Serial.print(" | LED: ");
+  Serial.println(ledValue);
+  Serial.println(" ");
+
+  analogWrite(LED_BUILTIN, ledValue);
+  ledBrightness = (ledBrightness + 1) % 256;
 }
 
 // put function definitions here:
