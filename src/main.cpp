3941cd49463fd16fe9218ff9ffbc3b268fc0aa6b
Zupacabra <zupanm@msoe.edu>
2025-09-10 12:12:49 -0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,8 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
-
 void setup() {
-  Serial.begin(115200); // other common is 9600 - this is comm speed
+  Serial.begin(115200); // other common is 9600 - this is Serial comm speed
   Serial.println("Starting Reads");
-  digitalWrite(LED_BUILTIN, 1);
 }
 
 int ledBrightness = 0;
@@ -14,7 +10,7 @@ int ledValue = 0;
 
 void loop() {
   delay(250);
-  int reading = analogRead(A0);
+  int reading = analogRead(A0); // Photo resistor on A0 w/ 2.2kOhm resistor to ground
   ledValue = 255 - (int)((reading / 4095.0) * 255);
 
   Serial.print("Result: ");
@@ -25,9 +21,4 @@ void loop() {
 
   analogWrite(LED_BUILTIN, ledValue);
   ledBrightness = (ledBrightness + 1) % 256;
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
 }
